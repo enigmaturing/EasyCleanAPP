d3986341eda9ccea599d00416047c2146146a422
Javier Gonzalez <javier.glez.martin@gmail.com>
2020-01-14 12:02:48 +0100

    Add class RegisterModel and EndClient
    (Nr. 35) These two classes serve as model for the communication with our SQL DB in azure

---
+++ EasyClean/EasyClean/Models/EndClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EasyClean.Models
+{
+    class EndClient
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string UserSurname { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+        public int DateOfBirth { get; set; }
+        public string ImagePath { get; set; }
+        public object ImageArray { get; set; }
+    }
+}
